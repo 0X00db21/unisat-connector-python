e7ad1df98f96ae957eef23e5c3803c4eda7ccdd7
0xdb21 <git@0xdb21.me>
2023-11-22 14:54:34 +0100

    Update docstrings for BRC20.get_best_block_height to align with docs.unisat.io documentation

--- unisat/UDS/brc20.py
+++ unisat/UDS/brc20.py
@@ -7,6 +7,9 @@ class BRC20:
         self.client = client
 
     def get_best_block_height(self):
-        """Get the best block height of BRC20 data."""
+        '''Get the best block height of BRC20 data.
+        This value will be consistend with the latest block height a short
+        time after the block has been confirmed.
+        '''
         route = '/v1/indexer/brc20/bestheight'
         return self.client.call(method='GET', route=route)
