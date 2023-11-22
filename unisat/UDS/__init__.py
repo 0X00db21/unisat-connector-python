a3a8a1aed39d96d8e100f64a9dba7d82c7118d2c
0xdb21 <git@0xdb21.me>
2023-11-22 16:36:15 +0100

    Add new module - initialize General module

--- unisat/UDS/__init__.py
+++ unisat/UDS/__init__.py
@@ -1,5 +1,7 @@
 from .brc20 import BRC20
+from .general import General
 
 class UDS:
     def __init__(self, client):
         self.brc20 = BRC20(client)
+        self.general = General(client)
---
+++ unisat/UDS/general.py
@@ -0,0 +1,7 @@
+class General:
+    ''' Implementation of general Module of UniSat API (from UDS documentations)
+    see: https://docs.unisat.io/dev/unisat-developer-service/general
+    '''
+
+    def __init__(self, client):
+        self.client = client
